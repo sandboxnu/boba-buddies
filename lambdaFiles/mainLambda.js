45a8f801b4f34984400140905d32150fff31c693
Justine <jluo4663@gmail.com>
2022-04-05 23:17:41 -0400

    add resource paths

--- lambdaFiles/mainLambda.js
+++ lambdaFiles/mainLambda.js
@@ -1,3 +1,7 @@
+const AWS = require("aws-sdk");
+const S3 = new AWS.S3();
+const config = require("./config.json");
+
 const https = require("https"),
   qs = require("querystring"),
   ACCESS_TOKEN = process.env.BOT_TOKEN;
@@ -19,28 +23,56 @@ function sendMessage(text, channel) {
   https.get(options);
 }
 
+function putObjectInS3(text) {
+  var s3 = new AWS.S3();
+  var params = {
+    Bucket: config.S3_BUCKET_NAME,
+    Key: "boba-buddies/text.json",
+    Body: JSON.stringify({ text }),
+    ContentType: "application/json; charset=utf-8",
+  };
+  console.log(text);
+  s3.putObject(params, (err, data) => {
+    if (err) console.log(err);
+    else console.log(data);
+  });
+}
+
 /**
  * resends the direct message being sent to it
  */
 function resendText(event, callback) {
   // test ensures that message isnt from a bot and is a direct message
+
+  console.log(event.bot_id, !event.bot_id);
   if (!event.bot_id && event.channel_type === "im") {
     const { text, channel } = event;
     sendMessage(text, channel);
+    putObjectInS3(text);
   }
   callback(null);
 }
 
+function handleInteractions(callback) {
+  console.log("pressed button");
+  callback(null, "ok");
+}
+
 exports.handler = (data, context, callback) => {
   console.log(data);
-  switch (data.type) {
-    case "url_verification":
-      verify(data, callback);
+  const resource = data.resource;
+
+  switch (resource) {
+    case "/event-handler":
+      const body = JSON.parse(data.body);
+      //resendText(body["event"], callback);
+      callback(null);
       break;
-    case "event_callback":
-      resendText(data.event, callback);
+    case "/interactive-handler":
+      handleInteractions(callback);
       break;
     default:
+      console.log("What is going on");
       callback(null);
   }
 };
