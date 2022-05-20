da1699b57fead960bd5656825145739ae869053f
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2022-05-19 21:49:29 -0400

    TODO

--- pairAndIntroduce.ts
+++ pairAndIntroduce.ts
@@ -9,5 +9,17 @@ const app = new App({
   appToken: process.env.SLACK_APP_TOKEN // add this
 });
 
+/*
+ TODO: implement garbage here
+ - check date relative to pairing date
+ - if been two weeks => new pairings!
+   - if first time pairing (ok this happens once) => generatePairs()
+   - else we just want to shift => shiftByOne()
+   - send pairs to startConversation()
+   - reset the pairing date
+ - it's been one week => check in!
+   - ok call the checkIn() and pray the lambda handles it
+ - someone figure out how to set some timer so this process doesn't run 10 times a second
+ */
 
 
